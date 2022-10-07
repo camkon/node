069ddec0e99b7481671f7174ea92af9ca761ed8a
Ansaf Nisam <80558066+camkon@users.noreply.github.com>
2022-10-07 08:44:29 +0530

    added comments on its working

--- 05 web-server/server.js
+++ 05 web-server/server.js
@@ -1,3 +1,17 @@
+/*
+working: GET
+1: server creates a local server using http and name the functions as 'server'
+2: listents to any emit events
+3: 'server' functions listens on the port for any requests:
+    3.1: it gets file requests from the user, logs the data to reqLog.txt
+    3.2: sets the contentType by checking the extention of the file from request url
+    3.3: sets the filePath with respect to the content type
+    3.4: check if file exits
+        3.4.1: serve file
+        3.4.2: redirect to 404 page
+*/
+
+
 const http = require('http')
 const path = require('path')
 const fs = require('fs')
@@ -10,6 +24,7 @@ const logEvents = require('./logEvents')
 class Emitter extends EventEmitter {}
 const myEmitter = new Emitter()
 
+//2
 myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName))
 
 const PORT = process.env.PORT || 3000
@@ -21,11 +36,11 @@ const serveFile = async (filePath, contentType, response) => {
         const rawData = await fsPromises.readFile(
             filePath, 
             // path.parse(filePath).ext != '.html' ?  '' : 'utf-8'
-            !contentType.includes('image') ? 'utf-8' : ''
+            !contentType.includes('image') ? 'utf-8' : '' //image does not go with utf-8, so blank
         )
         const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData
         response.writeHead(
-            filePath.includes('404.html') ? 404 : 200, 
+            filePath.includes('404.html') ? 404 : 200, //a switch statement would be better
             {'Content-Type' :  contentType}
         )
         response.end(contentType === 'application/json' ? JSON.stringify(data) : data)
@@ -38,7 +53,8 @@ const serveFile = async (filePath, contentType, response) => {
 }
 
 
-//server
+//1: server
+// 2.1
 const server = http.createServer((req, res) => {
 
     const extention = path.extname(req.url)
@@ -47,6 +63,7 @@ const server = http.createServer((req, res) => {
 
     let contentType;
 
+    //3.2
     switch(extention) {
         case '.css': contentType = 'text/css'; break;
         case '.js': contentType = 'text/javascript'; break;
@@ -57,6 +74,7 @@ const server = http.createServer((req, res) => {
         default: contentType = 'text/html'; break;
     }
 
+    //3.3
     if(contentType === 'text/html' && req.url === '/') {
         filePath = path.join(__dirname, 'views', 'index.html')
         console.log('1',filePath)
@@ -71,12 +89,13 @@ const server = http.createServer((req, res) => {
         console.log('4',filePath)
     }
                     
-    if(!extention && req.url.slice(-1) != '/') filePath += '.html'
+    if(!extention && req.url.slice(-1) != '/') filePath += '.html' //if the user didn't type 'fileName.html', automaticaly adds .html to the end
 
-    const fileExits = fs.existsSync(filePath)
+    const fileExits = fs.existsSync(filePath) //checking if the file exits. returns boolean
 
+    //3.4
     if(fileExits) {
-        serveFile(filePath, contentType, res)
+        serveFile(filePath, contentType, res) //3.4.1
     }else {
         console.log('redirect',fileExits, path.parse(filePath).base);
         switch(path.parse(filePath).base) {
@@ -87,7 +106,7 @@ const server = http.createServer((req, res) => {
                 serveFile(path.join(__dirname, 'data', 'data.html'), 'text/html', res);
                 break;
             default:
-                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res); //3.4.2
                 break;
 
         }
@@ -95,6 +114,7 @@ const server = http.createServer((req, res) => {
 
 })
 
+//3
 server.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 })
